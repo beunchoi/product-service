e028b7b5af62f29b55af6d52f66a64ac6786a500
beunchoi <byungeun0228@gmail.com>
2025-03-10 16:50:27 +0900

    refactor rabbitmq

--- src/main/java/com/hanghae/productservice/domain/product/config/RabbitMQConfig.java
+++ src/main/java/com/hanghae/productservice/domain/product/config/RabbitMQConfig.java
@@ -8,17 +8,27 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
+  @Value("${spring.rabbitmq.host}")
+  private String host;
+
+  @Value("${spring.rabbitmq.username}")
+  private String username;
+
+  @Value("${spring.rabbitmq.password}")
+  private String password;
+
   @Bean
   public CachingConnectionFactory rabbitConnectionFactory() {
-    CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
-    connectionFactory.setUsername("guest");
-    connectionFactory.setPassword("guest");
+    CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+    connectionFactory.setUsername(username);
+    connectionFactory.setPassword(password);
     return connectionFactory;
   }
 
